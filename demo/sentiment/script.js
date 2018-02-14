161e84e40e79a85d4158281bb513bca79605c480
Jesse Wu <6445988+jwu308@users.noreply.github.com>
2018-02-13 22:39:03 -0500

    management -> maintainability in the sentiment case study

--- demo/sentiment/script.js
+++ demo/sentiment/script.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
     setTimeout(function() {
     	$('#loading').css("display", "none");
     }, 2200)
+
+    $('#stock_name').on('keypress', function(e) {
+	    var code = e.keyCode || e.which;
+	    if(code==13){
+	        // Enter pressed... do anything here...
+	        document.location.href = "file:///Users/jessewu/Documents/Personal-Website/jessewu.github.com/demo/sentiment/templates/stock.html";
+	    }
+	});
 });
