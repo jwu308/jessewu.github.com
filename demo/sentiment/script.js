75361be5c07acf64a15d99f47b3c7451f2dc2c1f
Jesse Wu <6445988+jwu308@users.noreply.github.com>
2018-02-16 01:31:58 -0500

    click to view individual pictures instead of set of pictures

--- demo/sentiment/script.js
+++ demo/sentiment/script.js
@@ -10,4 +10,27 @@ $(document).ready(function() {
 	        document.location.href = "file:///Users/jessewu/Documents/Personal-Website/jessewu.github.com/demo/sentiment/templates/stock.html";
 	    }
 	});
+
+	$('#industries .industry').click(function() {
+	    // Enter pressed... do anything here...
+	    document.location.href = "file:///Users/jessewu/Documents/Personal-Website/jessewu.github.com/demo/sentiment/templates/list.html";
+	});
+
+	$('#actions #compare').click(function() {
+		if ($('#actions #compare').hasClass("non-exit")) {
+			$('#stock_section_inner').css("display", "none");
+			$('#stock_section').append("<img style='margin-top: 140px' src='../img/comparison.svg'/>");
+			$('#compare').attr("src", "../img/exit_comparison.svg");
+			$('#actions #compare').removeClass("non-exit");
+		}
+	});
+
+	$('#actions #read').click(function() {
+		if ($('#actions #read').hasClass("non-exit")) {
+			$('#stock_section_without_title').css("display", "none");
+			$('#stock_section_inner').append("<img style='margin-top: 40px' src='../img/the_news.svg'/>");
+			$('#read').attr("src", "../img/exit_the_news.svg");
+			$('#actions #read').removeClass("non-exit");
+		}
+	});
 });
