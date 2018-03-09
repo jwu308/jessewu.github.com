1117343c8ca0a9d31b816be62e9cdb81bf9cf022
Jesse Wu <6445988+jwu308@users.noreply.github.com>
2018-03-09 14:59:47 -0500

    design process added

--- demo/sentiment/script.js
+++ demo/sentiment/script.js
@@ -16,21 +16,33 @@ $(document).ready(function() {
 	    document.location.href = "file:///Users/jessewu/Documents/Personal-Website/jessewu.github.com/demo/sentiment/templates/list.html";
 	});
 
-	$('#actions #compare').click(function() {
-		if ($('#actions #compare').hasClass("non-exit")) {
-			$('#stock_section_inner').css("display", "none");
-			$('#stock_section').append("<img style='margin-top: 140px' src='../img/comparison.svg'/>");
-			$('#compare').attr("src", "../img/exit_comparison.svg");
-			$('#actions #compare').removeClass("non-exit");
-		}
-	});
-
 	$('#actions #read').click(function() {
-		if ($('#actions #read').hasClass("non-exit")) {
-			$('#stock_section_without_title').css("display", "none");
-			$('#stock_section_inner').append("<img style='margin-top: 40px' src='../img/the_news.svg'/>");
-			$('#read').attr("src", "../img/exit_the_news.svg");
-			$('#actions #read').removeClass("non-exit");
+		if ($(this).hasClass("clicked")) {
+			$(this).removeClass("clicked");
+
+			$(this).attr("src", "../img/read_the_news.svg");
+
+			$('#news').animate({
+				opacity: 0
+			}, 250, function () {
+				$('#single_stock').css("display", "block");
+				$('#single_stock').animate({
+					opacity: 1
+				}, 250);
+			});
+		} else { 
+			$(this).addClass("clicked");
+
+			$(this).attr("src", "../img/exit_the_news.svg");
+
+			$('#single_stock').animate({
+				opacity: 0
+			}, 250, function() {
+				$('#single_stock').css("display", "none");
+				$('#news').animate({
+					opacity: 1
+				}, 250);
+			});
 		}
 	});
 });
